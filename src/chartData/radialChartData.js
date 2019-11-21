839bd8f24b5835dc1b84a5a293e45df78964df6f
Binkxor <binkxor@gmail.com>
2019-11-20 20:20:47 -0800

    Formatting

--- src/chartData/radialChartData.js
+++ src/chartData/radialChartData.js
@@ -2,7 +2,7 @@ const radialChartData = {
   title: '2018 Growth',
   series: [ 76 ],
   sources: [
-    { name: '2019 indeed.com', link: 'https://www.indeed.com/career/software-engineer' }
+    { name: '2019 Indeed.com', link: 'https://www.indeed.com/career/software-engineer' }
   ]
 }
 
